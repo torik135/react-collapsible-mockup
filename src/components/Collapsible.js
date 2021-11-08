914eecb194f9e205bc865b4a4a7ab9c96f4ccbec
torik <riqibrekele@gmail.com>
2021-11-08 19:44:02 +0700

    add useRef for height and animation to work

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import './Collapsible.css'
 
 const Collapsible = (prop) => {
@@ -8,12 +8,31 @@ const Collapsible = (prop) => {
     // and using scrollHeight like this code below is error
     // docuemnt cant find element by id parent
     // because the div didnt exist yet
-    const scrollHeight = document.getElementById('parent').scrollHeight
+    // const scrollHeight = document.getElementById('parent').scrollHeight
+
+    const parentRef = useRef()
+
+    // current  = value inside parentRef
+    // which element use the parentRef
+    // if (parentRef.current) console.log(parentRef.current.scrollHeight)
 
     return (
         <div className="collapsible">
             <button className="toggle" onClick={() => setIsOpen(!isOpen)}>{prop.label}</button>
-            <div className="content-parent" id="parent">
+            <div
+                className="content-parent"
+                ref={parentRef}
+                // if element is open
+                // then height = content height (px)
+                // else height = 0 px
+                style={
+                    isOpen ?
+                        {
+                            height: parentRef.current.scrollHeight + "px"
+                        } : {
+                            height: "0px"
+                        }}
+            >
                 <div className="content">{prop.children}</div>
             </div>
         </div>
