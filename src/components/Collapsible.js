1a610696806bdeded463a5fd8733995a9867c883
torik <riqibrekele@gmail.com>
2021-11-08 19:34:26 +0700

    edit vanilla js scrollHeight (error)

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -4,10 +4,16 @@ import './Collapsible.css'
 const Collapsible = (prop) => {
     const [isOpen, setIsOpen] = useState(false)
 
+    // the animation is missing if using height = auto inside css file
+    // and using scrollHeight like this code below is error
+    // docuemnt cant find element by id parent
+    // because the div didnt exist yet
+    const scrollHeight = document.getElementById('parent').scrollHeight
+
     return (
         <div className="collapsible">
             <button className="toggle" onClick={() => setIsOpen(!isOpen)}>{prop.label}</button>
-            <div className={isOpen ? "content-parent show" : "content-parent"}>
+            <div className="content-parent" id="parent">
                 <div className="content">{prop.children}</div>
             </div>
         </div>
