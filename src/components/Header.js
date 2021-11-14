fcc035d024b2b0704615d3249ea6b2832662253a
torik <riqibrekele@gmail.com>
2021-11-14 19:18:45 +0700

    add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,8 @@ const Header = () => {
     return (
         <header className={headerWhite ? "header header-white" : "header header-yellow"} onClick={() => setHeaderWhite(!headerWhite)}>
             <h1>Just Header.</h1>
+            <h4>click the 4 square bellow to open the collapsible</h4>
+            {/* change to open to close w/ useState */}
         </header>
     )
 }
